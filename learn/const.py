2748297aee8dec4c60537b95a03ea75ff8a9c61b
Xu Xinran <xxr3376@gmail.com>
2014-07-02 00:30:15 +0800

    change to http

--- learn/const.py
+++ learn/const.py
@@ -2,10 +2,10 @@
 URL={
 	'login' : 'https://learn.tsinghua.edu.cn/MultiLanguage/lesson/teacher/loginteacher.jsp',
 	'course_all' : 'http://learn.tsinghua.edu.cn/MultiLanguage/lesson/student/MyCourse.jsp?typepage=2',
-	'file' : 'https://learn.tsinghua.edu.cn/MultiLanguage/lesson/student/download.jsp',		#课程文件
-	'homework' : 'https://learn.tsinghua.edu.cn/MultiLanguage/lesson/student/hom_wk_brw.jsp',		#课程作业
+	'file' : 'http://learn.tsinghua.edu.cn/MultiLanguage/lesson/student/download.jsp',		#课程文件
+	'homework' : 'http://learn.tsinghua.edu.cn/MultiLanguage/lesson/student/hom_wk_brw.jsp',		#课程作业
 	'homework_detail' : 'http://learn.tsinghua.edu.cn/MultiLanguage/lesson/student/hom_wk_detail.jsp', #作业详细
-	'discuss' : 'https://learn.tsinghua.edu.cn/MultiLanguage/public/bbs/gettalkid_student.jsp',		#课程讨论
+	'discuss' : 'http://learn.tsinghua.edu.cn/MultiLanguage/public/bbs/gettalkid_student.jsp',		#课程讨论
 	'discuss_detail' : 'http://learn.tsinghua.edu.cn/MultiLanguage/public/bbs/talk_reply_student.jsp',
     'base_URL' : 'http://learn.tsinghua.edu.cn',
 
